00e8e9ccc8cfe4bda829fd18eb86a57d77687dd8
leeee <le.giantsquid.wei@gmail.com>
2013-03-17 19:06:35 -0700

    xcode proj

---
+++ IMissYoButt/IMissYoButt/PZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PZAppDelegate.h
+//  IMissYoButt
+//
+//  Created by Le Wei on 3/17/13.
+//  Copyright (c) 2013 EZPZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IMissYoButt/IMissYoButt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IMissYoButt
+//
+//  Created by Le Wei on 3/17/13.
+//  Copyright (c) 2013 EZPZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PZAppDelegate class]));
+    }
+}
