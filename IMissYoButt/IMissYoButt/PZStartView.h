f16a0415b09cad56acf241d0ace122b491066868
leeee <le.giantsquid.wei@gmail.com>
2013-03-26 16:43:50 -0700

    created startview

---
+++ IMissYoButt/IMissYoButt/PZStartView.h
@@ -0,0 +1,13 @@
+//
+//  PZStartView.h
+//  IMissYoButt
+//
+//  Created by Le Wei on 3/26/13.
+//  Copyright (c) 2013 EZPZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PZStartView : UIView
+
+@end
---
+++ IMissYoButt/IMissYoButt/PZStartView.m
@@ -0,0 +1,13 @@
+//
+//  PZStartView.m
+//  IMissYoButt
+//
+//  Created by Le Wei on 3/26/13.
+//  Copyright (c) 2013 EZPZ. All rights reserved.
+//
+
+#import "PZStartView.h"
+
+@implementation PZStartView
+
+@end
